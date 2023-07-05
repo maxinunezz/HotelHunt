a860f7ec867c495ac403a90c4cc2d1d46d0403de
Julita <julia.franchi7@gmail.com>
2023-07-05 16:50:59 -0300

    pipe la cagaste

--- Client/src/Pages/LoginPage/LoginPage.tsx
+++ Client/src/Pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 const LogingPage =()=> {
     return (
-        <div className="bg-orange-400 text-3xl "> Aquí el login</div>
+        <div className="bg-red-800 text-3xl "> Aquí el login</div>
     )
 }
 export default LogingPage
