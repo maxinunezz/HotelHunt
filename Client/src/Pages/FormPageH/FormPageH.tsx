dd851f0473705ae1305f94bc919281e5c9d666ba
KingL <raulfeliped@hotmail.com>
2023-07-08 01:15:36 -0400

    featFormPageUser-faltacss

--- Client/src/Pages/FormPageH/FormPageH.tsx
+++ Client/src/Pages/FormPageH/FormPageH.tsx
@@ -7,6 +7,7 @@ import BackButton from '../../components/BackButton/BackButton';
 import { ArrowCounterClockwise } from '@phosphor-icons/react';
 import axios from 'axios';
 import { errorToast, successToast } from '../../components/toast';
+import Hotel from "./Hotel.png"
 
 interface FormValues {
 	name: string;
@@ -55,6 +56,7 @@ export default function FormPageH() {
 				successToast('Hotel creado correctamente');
 				console.log('data', data);
 			} catch (error) {
+
 				errorToast('Hubo un error, intenta de nuevo');
 			}
 			helpers.setSubmitting(false);
@@ -62,242 +64,241 @@ export default function FormPageH() {
 		[setIsCreated]
 	);
 
-	return (
-		<div className="flex h-screen">
-			<div className="w-full bg-blue-500 flex flex-col justify-center">
-				<h2 className="text-3xl text-white font-bold px-8 text-center">
-					Título del área azul
-				</h2>
-			</div>
-			<div className="w-full bg-gray-800 shadow-lg p-8 overflow-y-auto">
-				<div className="max-h-full">
-					<div className="flex justify-start">
-						<BackButton />
-					</div>
-					<h2 className="text-2xl font-bold mb-4 text-blue-500 text-center">
-						✨REGISTRO DE HOTELES✨
-					</h2>
-					{isCreated && (
-						<p className="text-green-500">El formulario se creó con éxito.</p>
-					)}
-					<Formik<FormValues>
-						enableReinitialize
-						initialValues={{
-							name: '',
-							description: '',
-							country: '',
-							city: '',
-							photo: '',
-							floorNumber: '',
-						}}
-						onSubmit={handleSubmit}
-						validationSchema={formValidationSchema}
-					>
-						{({ values, errors, setFieldValue, resetForm }) => {
-							return (
-								<Form>
-									<FormControl
-										validation={
-											values.name.length === 0
-												? 'none'
-												: errors.name
-												? 'invalid'
-												: 'valid'
-										}
-										className="mb-4"
-									>
-										<FormControl.Label className="text-white">
-											Nombre
-										</FormControl.Label>
-										<FormControl.Input
-											type="text"
-											placeholder="Nombre"
-											onChange={async (event) => {
-												await setFieldValue('name', event.target.value);
-											}}
-											value={values.name}
-											required
-										/>
-										<FormControl.Text>{errors.name}</FormControl.Text>
-									</FormControl>
-									<FormControl
-										validation={
-											values.description.length === 0
-												? 'none'
-												: errors.description
-												? 'invalid'
-												: 'valid'
-										}
-										className="mb-4"
-									>
-										<FormControl.Label className="text-white">
-											Descripcion
-										</FormControl.Label>
-										<FormControl.Textarea
-											placeholder="Descripcion"
-											onChange={async (event) => {
-												await setFieldValue('description', event.target.value);
-											}}
-											value={values.description}
-											className="h-40"
-											required
-										/>
-										<FormControl.Text>{errors.description}</FormControl.Text>
-									</FormControl>
-									<FormControl
-										validation={
-											values.country.length === 0
-												? 'none'
-												: errors.country
-												? 'invalid'
-												: 'valid'
-										}
-										className="mb-4"
-									>
-										<FormControl.Label className="text-white">
-											Pais
-										</FormControl.Label>
-										<FormControl.Input
-											type="text"
-											placeholder="Pais"
-											onChange={async (event) => {
-												await setFieldValue('country', event.target.value);
-											}}
-											value={values.country}
-											required
-										/>
-										<FormControl.Text>{errors.country}</FormControl.Text>
-									</FormControl>
-									<FormControl
-										validation={
-											values.city.length === 0
-												? 'none'
-												: errors.city
-												? 'invalid'
-												: 'valid'
-										}
-										className="mb-4"
-									>
-										<FormControl.Label className="text-white">
-											Ciudad
-										</FormControl.Label>
-										<FormControl.Input
-											type="text"
-											placeholder="Ciudad"
-											onChange={async (event) => {
-												await setFieldValue('city', event.target.value);
-											}}
-											value={values.city}
-											required
-										/>
-										<FormControl.Text>{errors.city}</FormControl.Text>
-									</FormControl>
-									{values.photo ? (
-										<div className="flex items-center justify-between mb-4">
-											<div className="w-10/12">
-												<img
-													src={values.photo}
-													alt="Imagen seleccionada"
-													className="max-h-full max-w-full"
-												/>
-											</div>
-											<button
-												type="button"
-												onClick={async () => {
-													await setFieldValue('photo', '');
-												}}
-												className="text-xl font-semibold text-red-500 hover:text-red-700 hover:underline focus:outline-none"
-											>
-												Borrar
-											</button>
-										</div>
-									) : (
+	return (		
+			<div className="flex h-screen">
+				<div className="w-full bg-blue-500 flex flex-col justify-center">
+					<img src={Hotel} alt="Imagen" className="mx-auto max-w-full" />
+					<h2 className="text-3xl text-white font-bold px-8 text-center">Título del área azul</h2>
+				</div>
+				<div className="w-full bg-gray-800 shadow-lg p-8 overflow-y-auto">
+					<div className="max-h-full">
+						<div className="flex justify-start">
+							<BackButton />
+						</div>
+						<h2 className="text-2xl font-bold mb-4 text-blue-500 text-center">
+							✨REGISTRO DE HOTELES✨
+						</h2>
+						{isCreated && (
+							<p className="text-green-500">El formulario se creó con éxito.</p>
+						)}
+						<Formik<FormValues>
+							enableReinitialize
+							initialValues={{
+								name: '',
+								description: '',
+								country: '',
+								city: '',
+								photo: '',
+								floorNumber: '',
+							}}
+							onSubmit={handleSubmit}
+							validationSchema={formValidationSchema}
+						>
+							{({ values, errors, setFieldValue, resetForm }) => {
+								return (
+									<Form>
 										<FormControl
 											validation={
-												values.photo.length === 0
+												values.name.length === 0
 													? 'none'
-													: errors.photo
-													? 'invalid'
-													: 'valid'
+													: errors.name
+														? 'invalid'
+														: 'valid'
 											}
 											className="mb-4"
 										>
 											<FormControl.Label className="text-white">
-												Foto
+												Nombre
 											</FormControl.Label>
 											<FormControl.Input
-												type="file"
-												placeholder="Foto"
-												accept="image/*"
+												type="text"
+												placeholder="Nombre"
 												onChange={async (event) => {
-													const file = event.target.files?.[0];
-													if (file) {
-														const reader = new FileReader();
-
-														reader.onload = async (
-															e: ProgressEvent<FileReader>
-														) => {
-															await setFieldValue(
-																'photo',
-																e.target?.result as string
-															);
-														};
-
-														reader.readAsDataURL(file);
-													}
+													await setFieldValue('name', event.target.value);
 												}}
-												value={values.photo}
+												value={values.name}
 												required
 											/>
-											<FormControl.Text>{errors.photo}</FormControl.Text>
+											<FormControl.Text>{errors.name}</FormControl.Text>
 										</FormControl>
-									)}
-									<FormControl
-										validation={
-											values.floorNumber.length === 0
-												? 'none'
-												: errors.floorNumber
-												? 'invalid'
-												: 'valid'
-										}
-										className="mb-4"
-									>
-										<FormControl.Label className="text-white">
-											Piso
-										</FormControl.Label>
-										<FormControl.Input
-											type="number"
-											placeholder="Piso"
-											onChange={async (event) => {
-												await setFieldValue('floorNumber', event.target.value);
-											}}
-											value={values.floorNumber}
-											required
-										/>
-										<FormControl.Text>{errors.floorNumber}</FormControl.Text>
-									</FormControl>
-									<div className="flex items-center justify-center">
-										<Button
-											className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md"
-											type="submit"
-											size="lg"
+										<FormControl
+											validation={
+												values.description.length === 0
+													? 'none'
+													: errors.description
+														? 'invalid'
+														: 'valid'
+											}
+											className="mb-4"
 										>
-											Registrar
-										</Button>
-										<Button
-											className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded-md ml-4"
-											onClick={() => resetForm()}
-											size="lg"
+											<FormControl.Label className="text-white">
+												Descripcion
+											</FormControl.Label>
+											<FormControl.Textarea
+												placeholder="Descripcion"
+												onChange={async (event) => {
+													await setFieldValue('description', event.target.value);
+												}}
+												value={values.description}
+												className="h-40"
+												required
+											/>
+											<FormControl.Text>{errors.description}</FormControl.Text>
+										</FormControl>
+										<FormControl
+											validation={
+												values.country.length === 0
+													? 'none'
+													: errors.country
+														? 'invalid'
+														: 'valid'
+											}
+											className="mb-4"
 										>
-											<ArrowCounterClockwise weight="bold" size={24} />
-										</Button>
-									</div>
-								</Form>
-							);
-						}}
-					</Formik>
+											<FormControl.Label className="text-white">
+												Pais
+											</FormControl.Label>
+											<FormControl.Input
+												type="text"
+												placeholder="Pais"
+												onChange={async (event) => {
+													await setFieldValue('country', event.target.value);
+												}}
+												value={values.country}
+												required
+											/>
+											<FormControl.Text>{errors.country}</FormControl.Text>
+										</FormControl>
+										<FormControl
+											validation={
+												values.city.length === 0
+													? 'none'
+													: errors.city
+														? 'invalid'
+														: 'valid'
+											}
+											className="mb-4"
+										>
+											<FormControl.Label className="text-white">
+												Ciudad
+											</FormControl.Label>
+											<FormControl.Input
+												type="text"
+												placeholder="Ciudad"
+												onChange={async (event) => {
+													await setFieldValue('city', event.target.value);
+												}}
+												value={values.city}
+												required
+											/>
+											<FormControl.Text>{errors.city}</FormControl.Text>
+										</FormControl>
+										{values.photo ? (
+											<div className="flex items-center justify-between mb-4">
+												<div className="w-10/12">
+													<img
+														src={values.photo}
+														alt="Imagen seleccionada"
+														className="max-h-full max-w-full"
+													/>
+												</div>
+												<button
+													type="button"
+													onClick={async () => {
+														await setFieldValue('photo', '');
+													}}
+													className="text-xl font-semibold text-red-500 hover:text-red-700 hover:underline focus:outline-none"
+												>
+													Borrar
+												</button>
+											</div>
+										) : (
+											<FormControl
+												validation={
+													values.photo.length === 0
+														? 'none'
+														: errors.photo
+															? 'invalid'
+															: 'valid'
+												}
+												className="mb-4"
+											>
+												<FormControl.Label className="text-white">
+													Foto
+												</FormControl.Label>
+												<FormControl.Input
+													type="file"
+													placeholder="Foto"
+													accept="image/*"
+													onChange={async (event) => {
+														const file = event.target.files?.[0];
+														if (file) {
+															const reader = new FileReader();
+
+															reader.onload = async (
+																e: ProgressEvent<FileReader>
+															) => {
+																await setFieldValue(
+																	'photo',
+																	e.target?.result as string
+																);
+															};
+
+															reader.readAsDataURL(file);
+														}
+													}}
+													value={values.photo}
+													required
+												/>
+												<FormControl.Text>{errors.photo}</FormControl.Text>
+											</FormControl>
+										)}
+										<FormControl
+											validation={
+												values.floorNumber.length === 0
+													? 'none'
+													: errors.floorNumber
+														? 'invalid'
+														: 'valid'
+											}
+											className="mb-4"
+										>
+											<FormControl.Label className="text-white">
+												Piso
+											</FormControl.Label>
+											<FormControl.Input
+												type="number"
+												placeholder="Piso"
+												onChange={async (event) => {
+													await setFieldValue('floorNumber', event.target.value);
+												}}
+												value={values.floorNumber}
+												required
+											/>
+											<FormControl.Text>{errors.floorNumber}</FormControl.Text>
+										</FormControl>
+										<div className="flex items-center justify-center">
+											<Button
+												className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md"
+												type="submit"
+												size="lg"
+											>
+												Registrar
+											</Button>
+											<Button
+												className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded-md ml-4"
+												onClick={() => resetForm()}
+												size="lg"
+											>
+												<ArrowCounterClockwise weight="bold" size={24} />
+											</Button>
+										</div>
+									</Form>
+								);
+							}}
+						</Formik>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+			);
 }
--- Client/src/Pages/FormPageUser/FormPageUser.tsx
+++ Client/src/Pages/FormPageUser/FormPageUser.tsx
@@ -1,7 +1,8 @@
 import { Button, FormControl } from '@rewind-ui/core';
 import { Form, Formik, FormikHelpers } from 'formik';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import * as yup from 'yup';
 
 interface UserCreateValues {
@@ -13,7 +14,7 @@ interface UserCreateValues {
     password: string;
 }
 
-const loginValidationSchema = yup.object().shape({
+const signValidationSchema = yup.object().shape({
     name: yup.string().trim().required('El nombre es requerido'),
     surname: yup.string().trim().required('El apellido es requerido'),
     dob: yup
@@ -26,22 +27,232 @@ const loginValidationSchema = yup.object().shape({
         .string()
         .trim()
         .email('El texto ingresado tiene que ser un email')
-        .required('El email es requerido'),
-    password: yup.string().trim().required('La contraseña es requerida'),
+        .required('El email es requerido')
+        .matches(/.com|.net|.org|.gov|.edu|.mil|.int|.io|.co|.us|.uk|.de|.fr|.ca|.au|.jp|.cn|.ru|.br|.it|.cl|.ar/, 'El email debe contener ".com"'),
+    password: yup
+        .string()
+        .required('Por favor, proporciona una contraseña válida')
+        .matches(/[a-z]/, 'La contraseña debe contener al menos una letra minúscula')
+        .matches(/[A-Z]/, 'La contraseña debe contener al menos una letra mayúscula')
+        .matches(/\d/, 'La contraseña debe contener al menos un número')
+        .min(8, 'La contraseña debe tener al menos 8 caracteres'), 
 });
 
 const FormPageUser = () => {
+    const [isCreated, setIsCreated] = useState(false);
+
     const handleSubmit = useCallback(
         async (values: UserCreateValues, helpers: FormikHelpers<UserCreateValues>) => {
-            console.log('values', values);
+            try {
+                const data = await axios.post('http://localhost:3001/user/signup', {
+                    name: values.name,
+                    surname: values.surname,
+                    dob: values.dob,
+                    phone: values.phone,
+                    email: values.email,
+                    password: values.password,
+                });
+                setIsCreated(true);
+                console.log('data', data);
+            } catch (error) {
+                console.log(error);
 
+            }
             helpers.setSubmitting(false);
         },
-        []
+        [setIsCreated]
     );
     return (
         <div className="w-[90%] bg-emerald-600 h-[90%]">
-            <Formik
+            {isCreated && (
+                <p className="text-green-500">El formulario se creó con éxito.</p>
+            )}
+            <Formik<UserCreateValues>
+                enableReinitialize
+                initialValues={{
+                    name: '',
+                    surname: '',
+                    dob: '',
+                    phone: '',
+                    email: '',
+                    password: '',
+                }}
+                onSubmit={handleSubmit}
+                validationSchema={signValidationSchema}
+            >
+                {({ values, errors, setFieldValue }) => (
+                    <Form>
+                        <FormControl
+                            validation={
+                                values.name.length === 0
+                                    ? 'none'
+                                    : errors.name
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Nombre
+                            </FormControl.Label>
+                            <FormControl.Input
+                                type="text"
+                                placeholder="Nombre"
+                                onChange={async (event) => {
+                                    await setFieldValue('name', event.target.value);
+                                }}
+                                value={values.name}
+                                required
+                            />
+                            <FormControl.Text>{errors.name}</FormControl.Text>
+                        </FormControl>
+
+
+                        <FormControl
+                            validation={
+                                values.surname.length === 0
+                                    ? 'none'
+                                    : errors.surname
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Apellido
+                            </FormControl.Label>
+                            <FormControl.Input
+                                placeholder="Apellido"
+                                onChange={async (event) => {
+                                    await setFieldValue('surname', event.target.value);
+                                }}
+                                value={values.surname}
+                                className="mb-40"
+                                required
+                            />
+                            <FormControl.Text>{errors.surname}</FormControl.Text>
+                        </FormControl>
+
+                        <FormControl
+                            validation={
+                                values.dob.length === 0
+                                    ? 'none'
+                                    : errors.dob
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Fecha de nacimiento
+                            </FormControl.Label>
+                            <FormControl.Input
+                                type="date"
+                                placeholder="Fecha de nacimiento"
+                                onChange={async (event) => {
+                                    await setFieldValue('dob', event.target.value);
+                                }}
+                                value={values.dob}
+                                required
+                            />
+                            <FormControl.Text>{errors.dob}</FormControl.Text>
+                        </FormControl>
+
+
+                        <FormControl
+                            validation={
+                                values.phone.length === 0
+                                    ? 'none'
+                                    : errors.phone
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Telefono
+                            </FormControl.Label>
+                            <FormControl.Input
+                                type="text"
+                                placeholder="Telefono"
+                                onChange={async (event) => {
+                                    await setFieldValue('phone', event.target.value);
+                                }}
+                                value={values.phone}
+                                required
+                            />
+                            <FormControl.Text>{errors.phone}</FormControl.Text>
+                        </FormControl>
+
+
+                        <FormControl
+                            validation={
+                                values.email.length === 0
+                                    ? 'none'
+                                    : errors.email
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Email
+                            </FormControl.Label>
+                            <FormControl.Input
+                                type="text"
+                                placeholder="Email"
+                                onChange={async (event) => {
+                                    await setFieldValue('email', event.target.value);
+                                }}
+                                value={values.email}
+                                required
+                            />
+                            <FormControl.Text>{errors.email}</FormControl.Text>
+                        </FormControl>
+
+
+                        <FormControl
+                            validation={
+                                values.password.length === 0
+                                    ? 'none'
+                                    : errors.password
+                                        ? 'invalid'
+                                        : 'valid'
+                            }
+                            className="mb-4"
+                        >
+                            <FormControl.Label className="text-white">
+                                Password
+                            </FormControl.Label>
+                            <FormControl.Input
+                                type="password"
+                                placeholder="Password"
+                                onChange={async (event) => {
+                                    await setFieldValue('password', event.target.value);
+                                }}
+                                value={values.password}
+                                required
+                            />
+                            <FormControl.Text>{errors.password}</FormControl.Text>
+                        </FormControl>
+
+                        <div className="flex items-center justify-center">
+                            <Button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-md"
+                                type="submit"
+                                size="lg"
+                            >
+                                Registrar
+                            </Button>
+
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </div >
+    );
+};
+{/* <Formik
                 initialValues={
                     {
                         name: "",
@@ -145,9 +356,7 @@ const FormPageUser = () => {
                     );
                 }}
 
-            </Formik>
-        </div >
-    )
-}
+            </Formik> */}
+
 
 export default FormPageUser;
