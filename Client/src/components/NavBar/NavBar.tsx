53fa97158f96d9b371eb174de140c5f4efd65e18
ManrrubiaF <122764112+ManrrubiaF@users.noreply.github.com>
2023-07-16 21:28:17 -0300

    Update NavBar.tsx

--- Client/src/components/NavBar/NavBar.tsx
+++ Client/src/components/NavBar/NavBar.tsx
@@ -4,7 +4,9 @@ import { hotelStore, searchStore, tokenStore } from "../../Store";
 import UserMenu from "../UserMenuDropDown/UserMenu";
 import AdminMenu from "../UserMenuDropDown/AdminMenu";
 import { Dropdown, Button, Input } from "@rewind-ui/core";
+import { UserCircle } from '@phosphor-icons/react';
 import { useEffect, useState } from "react";
+import CartComponent from '../CartComponent/CartComponent';
 
 export default function NavBar() {
   const navigate = useNavigate();
@@ -45,33 +47,39 @@ export default function NavBar() {
     navigate('/roomSearch')
   }
 
-
-
-
-
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-gray-900 text-white p-4 space-x-5 flex items-center justify-between w-full">
-      {isLogged.length ? (
-        isLogged[2] === "normal" ? (
-          <UserMenu />
-        ) : (
-          <AdminMenu />
-        )
-      ) : (
-        <h1 onClick={() => navigate("/login")}>Login</h1>
-      )}
+		<nav className="fixed top-0 left-0 right-0 bg-gray-900 text-white p-4 space-x-5 flex items-center justify-between w-full">
 
-      <SearchBar />
+			<div className="flex items-center">
+				<img
+					className="h-[70px] w-[80px] mr-2"
+					src="https://cdn.discordapp.com/attachments/1125503406371524661/1127923542743334912/image.png"
+					alt="Logo"
+				/>
+				<div>
+					<h1 className="text-white">
+						<span className="text-5xl font-bold tracking-wider">HOTEL</span>
+						<span className="text-blue-500 text-5xl font-extrabold tracking-wider">HUNT</span>
+					</h1>
+				</div>
+			</div>
+			<SearchBar />
+      <button onClick={handleRoomSearch} className="px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600">
+        Habitaciones</button>
 
+			<button
+				className="px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600"
+				onClick={handleAllHotels}
+			>
+				Mostrar todos los hoteles
+			</button>
 
-      <button onClick={handleRoomSearch}>Habitaciones</button>
-      <button onClick={handleAllHotels}>Mostrar todos los hoteles</button>
-      <Dropdown size="md">
-        <Dropdown.Trigger>
-          <Button className="w-500">{`Ordenar por: `}</Button>
-        </Dropdown.Trigger>
-        <Dropdown.Content>
-          <Dropdown.Item onClick={() => setOrderByNameState("ASC")}>
+			<Dropdown size='md'>
+				<Dropdown.Trigger>
+					<Button className="w-500">{`Ordenar por: `}</Button>
+				</Dropdown.Trigger>
+				<Dropdown.Content>
+        <Dropdown.Item onClick={() => setOrderByNameState("ASC")}>
             Ordenar por Nombre ASC
           </Dropdown.Item>
           <Dropdown.Item onClick={() => setOrderByNameState("DES")}>
@@ -82,22 +90,26 @@ export default function NavBar() {
           </Dropdown.Item>
           <Dropdown.Item onClick={() => setOrderByCategoryState("DES")}>
             Ordenar por Categoria DES
-          </Dropdown.Item>
-        </Dropdown.Content>
-      </Dropdown>
-      <div>
-        <h1 className="text-white">
-          <span onClick={() => navigate("/")} className="text-4xl font-bold">
-            HOTEL
-          </span>
-          <span
-            onClick={() => navigate("/")}
-            className="text-blue-500 text-4xl font-extrabold"
-          >
-            HUNT
-          </span>
-        </h1>
-      </div>
-    </nav>
-  );
+          </Dropdown.Item>					
+				</Dropdown.Content>
+			</Dropdown>
+
+			{
+				isLogged.length ? (
+					isLogged[2] === "normal" ? <UserMenu /> : <AdminMenu />)
+					:
+					(
+						<div className="flex items-center cursor-pointer" onClick={() => navigate('/login')}>
+							<UserCircle size={32} className="mr-2" />
+							<h1>Login</h1>
+						</div>
+					)
+			}
+        <CartComponent/>
+
+		</nav>
+	);
+
+  
 }
+
