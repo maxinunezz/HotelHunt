0cc4a7a2b2c5f26f40e7696fb3838244b0137dcb
maxinunezz <maxicbapl@gmail.com>
2023-07-20 16:57:34 -0300

    favhotel

--- Client/src/Store/HotelsStore.ts
+++ Client/src/Store/HotelsStore.ts
@@ -1,95 +1,93 @@
-import axios from 'axios';
-import { create } from 'zustand';
-import { Hotel } from '../models';
+import axios from "axios";
+import { create } from "zustand";
+import { Hotel } from "../models";
 const url = import.meta.env.VITE_URL;
 
-
 type States = {
-	hotels: Hotel[];
-	currentPage: number;
-	pageNumbers: number[];
+  hotels: Hotel[];
+  currentPage: number;
+  pageNumbers: number[];
+  favoriteHotel: Hotel[];
 };
 
 type Actions = {
-	fetchHotels: () => Promise<void>;
-	setCurrentPage: (pageNum: number) => void;
-	orderByName: (array: Hotel[], event: string) => void;
-	orderByCategory: (array: Hotel[], event: string) => void;
-	resetHotels: () => void;
+  fetchHotels: () => Promise<void>;
+  setCurrentPage: (pageNum: number) => void;
+  orderByName: (array: Hotel[], event: string) => void;
+  orderByCategory: (array: Hotel[], event: string) => void;
+  resetHotels: () => void;
+  addFavorite: () => void;
+  deleteFavorite: () => void;
 };
 
 const initialState: States = {
-	hotels: [],
-	currentPage: 1,
-	pageNumbers: [],
+  hotels: [],
+  currentPage: 1,
+  pageNumbers: [],
+  favoriteHotel: [],
 };
 
 export const hotelStore = create<States & Actions>((set) => ({
-	...initialState,
+  ...initialState,
 
-	fetchHotels: async () => {
-		const { data } = await axios.get<Hotel[]>(`${url}/hotel`);
+  fetchHotels: async () => {
+    const { data } = await axios.get<Hotel[]>(`${url}/hotel`);
 
-		set((state) => ({
-			...state,
-			hotels: data,
-		}));
-	},
+    set((state) => ({
+      ...state,
+      hotels: data,
+    }));
+  },
 
-	setCurrentPage: (pageNum) => {
-		set(() => ({
-			currentPage: pageNum
-		}))
-	},
-	orderByName: (array, event) => {
+  setCurrentPage: (pageNum) => {
+    set(() => ({
+      currentPage: pageNum,
+    }));
+  },
+  orderByName: (array, event) => {
+    if (event === "ASC") {
+      const arrayAux = array.sort(function (a, b) {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+      set((state) => ({
+        ...state,
+        hotels: arrayAux,
+      }));
+    } else if (event === "DES") {
+      const arrayAux = array.sort((a, b) => b.name.localeCompare(a.name));
+      set((state) => ({
+        ...state,
+        hotels: arrayAux,
+      }));
+    }
+  },
+  orderByCategory: (array, event) => {
+    if (event === "ASC") {
+      const arrayAux = array.sort(
+        (a, b) => parseInt(a.hotelCategory) - parseInt(b.hotelCategory)
+      );
+      set((state) => ({
+        ...state,
+        hotels: arrayAux,
+      }));
+    } else if (event === "DES") {
+      const arrayAux = array.sort(
+        (a, b) => parseInt(b.hotelCategory) - parseInt(a.hotelCategory)
+      );
+      set((state) => ({
+        ...state,
+        hotels: arrayAux,
+      }));
+    }
+  },
 
-
-
-		if (event === 'ASC') {
-			const arrayAux = array.sort(function (a, b) {
-				if (a.name > b.name) {
-					return 1
-				}
-				if (a.name < b.name) {
-					return -1
-				}
-				return 0
-			})
-			set((state) => ({
-				...state,
-				hotels: arrayAux,
-			}));
-		}
-
-		else if (event === 'DES') {
-			const arrayAux = array.sort((a, b) => b.name.localeCompare(a.name));
-			set((state) => ({
-				...state,
-				hotels: arrayAux,
-			}))
-
-		}
-	},
-	orderByCategory: (array, event) => {
-		if (event === 'ASC') {
-			const arrayAux = array.sort((a, b) => parseInt(a.hotelCategory) - parseInt(b.hotelCategory));
-			set((state) => ({
-				...state,
-				hotels: arrayAux,
-			}));
-		} else if (event === 'DES') {
-			const arrayAux = array.sort((a, b) => parseInt(b.hotelCategory) - parseInt(a.hotelCategory));
-			set((state) => ({
-				...state,
-				hotels: arrayAux,
-			}));
-		}
-	},
-
-
-	resetHotels: () => {
-		set(initialState);
-	},
-
-
+  resetHotels: () => {
+    set(initialState);
+  },
 }));
--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,28 @@
-const server = require('./src/server');
-const { conn } = require('./src/db.js');
+const server = require("./src/server");
+const { conn } = require("./src/db.js");
 const PORT = 3001;
-const routes = require('./src/Routes/index.js');
-const cron = require('node-cron');
-const { scheduledDeletion, delInactiveAcc } = require('./src/Utils/cronJobs');
-const { firstload } = require('./src/Utils/Utils.js'); // descomentar para primera carga */
+const routes = require("./src/Routes/index.js");
+const cron = require("node-cron");
+const { scheduledDeletion, delInactiveAcc } = require("./src/Utils/cronJobs");
+const { firstload } = require("./src/Utils/Utils.js"); // descomentar para primera carga */
 
-server.use('/', routes);
-cron.schedule('0 0 * * *', async () => {
-	scheduledDeletion();
+server.use("/", routes);
+cron.schedule("0 0 * * *", async () => {
+  scheduledDeletion();
 });
-cron.schedule('0 */1 * * *', async() =>{
-	delInactiveAcc();
-})
+cron.schedule("0 */1 * * *", async () => {
+  delInactiveAcc();
+});
 
 conn
-	.sync({ force: true })
-	.then(() => {
-		/* cambiar a true en primera carga */
+  .sync({ force: true })
+  .then(() => {
+    /* cambiar a true en primera carga */
 
-		firstload(); // descomentar para primera carga
+    firstload(); // descomentar para primera carga
 
-		server.listen(PORT, () => {
-			console.log(`Server listening on port ${PORT}`);
-		});
-	})
-	.catch((error) => console.error(error));
-
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
