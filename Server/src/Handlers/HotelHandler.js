83f5b7e6a2188ef4681c0cfb168248655f4b385a
maxinunezz <maxicbapl@gmail.com>
2023-07-24 23:02:33 -0300

    trying deploy

--- Server/src/Handlers/HotelHandler.js
+++ Server/src/Handlers/HotelHandler.js
@@ -3,8 +3,7 @@ const { Hotel, conn } = require("../db");
 const getAllhotels = async (req, res) => {
   let hotels_array = [];
   try {
-
-    const data = await Hotel.findAll({where: {disabled: false}});
+    const data = await Hotel.findAll({ where: { disabled: false } });
     if (data.length === 0) {
       throw Error("No se encontraron hoteles");
     }
@@ -30,7 +29,6 @@ const getAllhotels = async (req, res) => {
   }
 };
 
-
 const updateHotel = async (req, res) => {
   const { id } = req.params;
   try {
@@ -38,11 +36,11 @@ const updateHotel = async (req, res) => {
     if (!hotel) {
       return res.status(404).send("Hotel no encontrado");
     }
-		await hotel.update(req.body);
-		return res.status(200).json(hotel);
-	} catch (error) {
-		return res.status(500).send(error.message);
-	}
+    await hotel.update(req.body);
+    return res.status(200).json(hotel);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 const deleteHotel = async (req, res) => {
