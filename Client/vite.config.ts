5b7835ea0835e38cd59897c5ce3c5f1742fe8302
maxinunezz <maxicbapl@gmail.com>
2023-07-25 19:48:56 -0300

    deploy

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     react(),
   ],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
