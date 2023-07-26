848892fd2ee7034ca6f6e7fc60e8b5fd958bcbfa
matsan <matiasanchez999@gmail.com>
2023-07-26 18:09:22 -0300

    booking3

--- Server/src/server.js
+++ Server/src/server.js
@@ -8,23 +8,9 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cookieParser());
-server.use(express.urlencoded({ extended: true }));
-const corsOptions = {
-    origin: ['http://localhost:5173/', 'https://hotelhunt.kerchakpetshop.com.ar/', 'https://oauth2.googleapis.com/'],
-    methods: ['GET', 'POST'],
-    credentials: true,
-  };
-server.use(cors(corsOptions));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+
 server.use(router);
 
 
