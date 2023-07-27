8f3ca4ed40f517796b8805a7994003a2e90bc0c8
ManrrubiaF <122764112+ManrrubiaF@users.noreply.github.com>
2023-07-27 02:56:21 -0300

    Delete Client directory

--- Client/src/Store/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './HotelsStore';
-export * from './SearchStore';
-export * from "./RoomsStores"
-export * from "./RoomSearchStore"
-export * from "./DashboardStore"
-export * from "./SAStore";
-export * from "./TokenStore";
--- Client/src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './use-toggle';
-export * from './use-fetch-hotels';
-export * from './use-fetch-rooms';
-export * from './use-fetch-roomsIdHotel';
-export * from './use-fetch-roomDetailSetter';
